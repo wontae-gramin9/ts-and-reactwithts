567284b49c5c78715ef3939a3e7031a67bce07d9
Wontae <wontae.gramin9@gmail.com>
2024-07-31 11:13:38 +0900

    [React Ts] String literal

---
+++ react-with-ts/src/App.tsx
@@ -0,0 +1,39 @@
+import ListOfNestedObject from "./components/props/ListOfNestedObject";
+import { NestedObject } from "./components/props/NestedObject";
+import { Object } from "./components/props/Object";
+import { Status } from "./components/props/StringLiteral";
+
+const nameList = [
+  {
+    first: "Bruce",
+    last: "Wayne",
+  },
+  {
+    first: "Clark",
+    last: "Kent",
+  },
+  {
+    first: "Princess",
+    last: "Diana",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Props</h1>
+      <Object name="Wontae" messageCount={5} isLoggedIn={true}></Object>
+      <NestedObject
+        name={{
+          first: "Clark",
+          last: "Diana",
+        }}
+      ></NestedObject>
+      <ListOfNestedObject names={nameList}></ListOfNestedObject>
+      <p>----------</p>
+      <Status status="success" />
+    </div>
+  );
+}
+
+export default App;
